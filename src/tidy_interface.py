4641155d096a16bea004a1d766c2e8e74bc7bc72
Amauri Lopez <lopeza16@tcnj.edu>
2018-10-30 15:59:39 -0400

    Backend for updating file extension dropdown on directory select

--- src/tidy_interface.py
+++ src/tidy_interface.py
@@ -1,7 +1,23 @@
 from tkinter import filedialog
 from tkinter import *
 import json
+import sys
+import os
 
+def update_ext_dropdown(func):
+    #decorator to update dropdown menu for file extensions
+    def wrapper(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+
+        #Delete existing drop down options
+        menu = self.ext_dropdown['menu']
+        menu.delete(0, 'end')
+
+        #Re-establish new drop down options
+        for ext in self.file_extensions:
+            menu.add_command(label=ext, command=lambda value=ext: self.target_extension.set(value))
+
+    return wrapper
 
 class TidyInterface():
 
@@ -17,11 +33,12 @@ class TidyInterface():
         #Attributes that may be used throughout class
         self._target_directory = StringVar()
         self.target_extension = StringVar()
-        self.alphabetical = IntVar().set(0)
-        self.by_date = IntVar().set(0)
-        self.sub_folders = IntVar().set(0)
+        self.alphabetical = IntVar()
+        self.by_date = IntVar()
+        self.sub_folders = IntVar()
         self.file_extensions = {''}
-        self.ext_dropdown = OptionMenu(frame, self.target_extension, *self.file_extensions).grid(column=4, row=2)
+        self.ext_dropdown = OptionMenu(frame, self.target_extension, *self.file_extensions)
+        self.ext_dropdown.grid(column=4, row=2)
 
         #Set up the top menu configuration for gui
         menu = Menu(self._master)
@@ -31,8 +48,8 @@ class TidyInterface():
         menu.add_cascade(label='File', menu=fileMenu)
         fileMenu.add_command(label='Open Criteria...', command=self.load_criteria)
         fileMenu.add_separator()
-        fileMenu.add_command(label='Save', command=self.save_criteria)
-        fileMenu.add_command(label='Save Criteria As...', command=self.save_criteria(save_as=True))
+        fileMenu.add_command(label='Save', command=self.save_criteria(self._criteria))
+        fileMenu.add_command(label='Save Criteria As...', command=self.save_criteria(self._criteria, save_as=True))
         fileMenu.add_separator()
         fileMenu.add_command(label='Exit', command=self._master.destroy)
 
@@ -51,7 +68,6 @@ class TidyInterface():
         chk_by_date = Checkbutton(frame, text="By Date", variable=self.by_date).grid(row=2, column=2, sticky=E)
         chk_sub_folders = Checkbutton(frame, variable = self.sub_folders).grid(row=3, column=1, sticky=W)
 
-
     def start(self):
         self._master.mainloop()
 
@@ -61,35 +77,35 @@ class TidyInterface():
         self._target_directory.set(path)
 
         #get file extensions in directory and update extensions drop down
-        self.file_extensions = self.get_file_extensions(path)
-        self.update_ext_dropdown()
-
-    def get_file_extensions(self, path):
-        #Getset of file extensions that exist within directory path
-        pass
-
-    def update_ext_dropdown(self):
-        #Delete existing drop down options
-        menu = self.ext_dropdown['menu']
-        menu.delete(0, 'end')
-
-        #Re-establish new drop down options
-        for ext in self.file_extensions:
-            menu.add_command(label=ext, command=lambda value=ext: self.target_extension.set(value))
-
+        self.update_file_extensions(self.get_file_extensions(path))
 
     def organize(self):
+        #method used to testing purposes
         print('working')
+        self.alphabetical.set(1)
 
-    def generate_criteria(self):
+    def load_criteria(self):
+        pass
+
+    @update_ext_dropdown
+    def update_file_extensions(self, extensions):
+        self.file_extensions = extensions
+
+    @staticmethod
+    def get_file_extensions(path):
+        #return set of file extensions that exist within directory path
+        extensions = set()
+        for file in os.listdir(path):
+            extensions.add(f".{file.split('.')[-1]}")
+
+        return extensions
+
+    @staticmethod
+    def save_criteria(criteria, save_as=False):
         #Generation of criteria .json files
         #including 'load' and 'save' functionality to save or load an existing criteria into the GUI
         #for future usage
         pass
 
-    def load_criteria(self):
-        pass
     
-    def save_criteria(self, save_as=False):
-        pass
 
